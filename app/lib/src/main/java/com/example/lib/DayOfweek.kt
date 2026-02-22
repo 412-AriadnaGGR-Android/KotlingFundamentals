0b0946f806bc64fecdd6c855fe9d56b1f1d83ea1
Ariadna Georgina Gomez Robledo <aribb400@gmail.com>
2026-02-22 00:42:15 -0600

    haciendo un cambio

--- app/lib/src/main/java/com/example/lib/DayOfweek.kt
+++ app/lib/src/main/java/com/example/lib/DayOfweek.kt
@@ -1,2 +1,31 @@
 package com.example.lib
+/*
+1,2,3,4,5,6,7
+* Escribir un programa que e muestreen consola los dias de la semana
+Y si es sabado o dmingo que escriba fin de semana
+*/
+fun main(){
+    val dayOfWeek= 2
+    //eso  es para comentar rapido seleccionas y das estos comandos
+    //Ctrl K Ctrl C /Cmd K Cmd C (MAC)
+    //Ctrl K Ctrl U /Cmd K Cmd U (MAC)
+//    if (dayOfWeek == 1){
+//        println("Es lunes")
+//    }else if (dayOfWeek == 2){
+//        println("Es martes")
+//    }
 
+    when(dayOfWeek){
+        1 ->println("Es lunes")
+        2->{
+            println("Es martes")
+            println("Haciendo cositas")
+        }
+        3->println("Es miercoles")
+        4->println("Es jueves")
+        5->println("Es viernes")
+        6,7->println("Es fin d semana")
+        else -> println("no es u dia de la semana ")
+
+    }
+}
