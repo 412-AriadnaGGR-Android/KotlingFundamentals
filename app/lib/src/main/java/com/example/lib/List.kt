ff4c8febe73cb9e9c3ad0dc0b9281ce60b5a94da
Ariadna Georgina Gomez Robledo <aribb400@gmail.com>
2026-02-22 00:47:35 -0600

    Guardando mis archivos

--- app/lib/src/main/java/com/example/lib/List.kt
+++ app/lib/src/main/java/com/example/lib/List.kt
@@ -1,5 +1,18 @@
 package com.example.lib
 
 fun main(){
+    /*
+    * Dada un lita d numeros desordenados, encuentra e imprime cul es el
+    * numero mas grande SIN usar la funcion MAX
+    * */
 
+    val numbers=listOf(1,4,4,3,2,6,7,8,9,10,23,23,12)
+    val numeroX = numbers [2]
+    var maxNumber=numbers[0]
+    for (number in numbers){
+        if (number > maxNumber){
+            maxNumber = number
+        }
+    }
+    println("el numero mas rande es $maxNumber")
 }
--- app/lib/src/main/java/com/example/lib/MyClass.kt
+++ app/lib/src/main/java/com/example/lib/MyClass.kt
@@ -4,4 +4,31 @@ fun main() {
     val name : String="Juan"
     var age : Int=28
     age=29
-}
+
+    //For
+    /** *for(int i = 0; i<10 ; i++) {
+     * }
+     *
+     * */
+    for (i in 1..10){
+        println(i)
+    }
+    for (i in 100 downTo 0 step 10){
+        println(i)
+    }
+
+    //Calculadora de propinas..
+    /**
+     * Crea un programa que tega un precio de comida y un
+     * porcentaje de propina.El programa debe imprimir
+     * cuantoesdepropins y cunto es el total a pagar
+     * */
+
+    //String Interpolaion
+    val cost = 500.0
+    val porcentaje = 0.15
+    val  tip = cost * porcentaje
+    val total= cost + tip
+    println("El total es  $total de pesos")
+    println("La propna es $tip")
+ }
